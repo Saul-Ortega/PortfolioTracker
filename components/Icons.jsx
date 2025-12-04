cf03100490ffab10864f3c9e8a5afb9b82aacf46
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2025-12-04 12:36:26 +0000

    Add icons file and Main component

---
+++ components/Icons.jsx
@@ -0,0 +1,13 @@
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+export const HomeIcon = (props) => (
+    <MaterialIcons name="home" size={24} color="black" {...props} />
+)
+
+export const UserIcon = (props) => (
+    <MaterialIcons name="verified-user" size={24} color="black" {...props} />
+)
+
+export const PasswordIcon = (props) => (
+    <MaterialIcons name="password" size={24} color="black" {...props} />
+)
---
+++ components/Main.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import "../global.css";
+
+export default function Main() {
+    return(
+        <View className="flex-1 justify-center items-center">
+            <Text className="text-black">Hola</Text>
+        </View>
+    );
+}
