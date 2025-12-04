e795635df67aee4ddbd43512074f3cdd765816c6
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2025-12-04 12:38:24 +0000

    Modify App

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
-import "./global.css"
+import { View } from 'react-native';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <StatusBar style="auto" />
-      <Text className="text-black">Open up App.js to start working on your app!</Text>
-    </View>
+
+      <View>
+        <StatusBar animated={true} />
+      </View>
+
   );
 }
