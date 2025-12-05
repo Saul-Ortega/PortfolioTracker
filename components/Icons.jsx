e1f4f9f8266a3a664e8d7c09e477756985e6ed37
Saul-Ortega <saulalbertoortegarodriguez@gmail.com>
2025-12-05 21:13:09 +0000

    Add of more Icons

--- components/Icons.jsx
+++ components/Icons.jsx
@@ -11,3 +11,23 @@ export const UserIcon = (props) => (
 export const PasswordIcon = (props) => (
     <MaterialIcons name="password" size={24} color="black" {...props} />
 )
+
+export const ProfileIcon = (props) => (
+    <MaterialIcons name="man-4" size={24} color="black" {...props} />
+)
+
+export const PortfolioIcon = (props) => (
+    <MaterialIcons name="account-balance" size={24} color="black" {...props} />
+)
+
+export const SearchIcon = (props) => (
+    <MaterialIcons name="search" size={24} color="black" {...props} />
+)
+
+export const AddIcon = (props) => (
+    <MaterialIcons name="add" size={24} color="black" {...props} />
+)
+
+export const CloseIcon = (props) => (
+    <MaterialIcons name="close" size={24} color="black" {...props} />
+)
